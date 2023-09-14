c2415cc20b85a44e7a1290a81f3d6c58a7cdd912
Kim Junhyuk <jkim1202@gachon.ac.kr>
2023-09-14 19:25:23 +0900

    feat: StudyTime Entity 생성

--- src/main/java/com/mmos/mmos/src/domain/entity/StudyTime.java
+++ src/main/java/com/mmos/mmos/src/domain/entity/StudyTime.java
@@ -1,18 +1,37 @@
 package com.mmos.mmos.src.domain.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+
+import java.sql.Timestamp;
 
 @Entity
 @Getter
 @NoArgsConstructor
 public class StudyTime {
 
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long studytime_index;
+    @Column
+    @ColumnDefault("current_timestamp")
+    private Timestamp studytime_start_time;
+
+    @Column
+    @ColumnDefault("current_timestamp on update current_timestamp")
+    private Timestamp studytime_end_time;
+
+    @Column
+    @ColumnDefault("true")
+    private boolean studytime_status;
+
+    @ManyToOne
+    @JoinColumn(name = "plan_index")
+    private Plan plan;
+
+
+
 }
