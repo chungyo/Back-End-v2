5f42bb8ee4112ca43c768b0f8b8dadf5cbe62045
KoungQ <guu119@naver.com>
2023-09-14 18:00:44 +0900

    feat: Major Entity 생성

--- src/main/java/com/mmos/mmos/src/domain/entity/Major.java
+++ src/main/java/com/mmos/mmos/src/domain/entity/Major.java
@@ -1,19 +1,29 @@
 package com.mmos.mmos.src.domain.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.DynamicInsert;
 
 @Entity
 @Getter
 @NoArgsConstructor
+@DynamicInsert
 public class Major {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long major_index;
 
+    @Column
+    private Long major_name;
+
+    @Column
+    @ColumnDefault("true")
+    private boolean major_status;
+
+    @ManyToOne
+    @JoinColumn(name = "univ_index")
+    private University university;
 }
