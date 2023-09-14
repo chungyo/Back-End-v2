05554ab7c7d97a71e8b2107ea6ab549b9a1b282b
KoungQ <guu119@naver.com>
2023-09-14 18:00:55 +0900

    feat: University Entity 생성

--- src/main/java/com/mmos/mmos/src/domain/entity/University.java
+++ src/main/java/com/mmos/mmos/src/domain/entity/University.java
@@ -1,18 +1,31 @@
 package com.mmos.mmos.src.domain.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicInsert;
+
+import java.util.List;
 
 @Entity
 @Getter
 @NoArgsConstructor
+@DynamicInsert
 public class University {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long university_index;
+
+    @Column
+    private String university_name;
+
+    @Column
+    private String university_status;
+
+    @OneToMany(mappedBy = "university", cascade = CascadeType.REMOVE, orphanRemoval = true)
+    private List<User> university_users;
+
+    @OneToMany(mappedBy = "university", cascade = CascadeType.REMOVE, orphanRemoval = true)
+    private List<Major> university_majors;
 }
