ad931735b7cb6cbc62abbbe47b546db924a78029
KoungQ <guu119@naver.com>
2023-09-14 20:24:50 +0900

    feat: Study Entity 생성

--- src/main/java/com/mmos/mmos/src/domain/entity/Study.java
+++ src/main/java/com/mmos/mmos/src/domain/entity/Study.java
@@ -3,6 +3,7 @@ package com.mmos.mmos.src.domain.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
 
 import java.util.List;
 
@@ -16,5 +17,31 @@ public class Study {
     private Long study_index;
 
     @OneToMany(mappedBy = "study", cascade = CascadeType.REMOVE, orphanRemoval = true)
-    private List<UserStudy> user_studies;
+    private List<UserStudy> study_userstudies;
+
+    @OneToMany(mappedBy = "study", cascade = CascadeType.REMOVE, orphanRemoval = true)
+    private List<User> study_users;
+
+    @Column
+    @ColumnDefault("null")
+    @OneToMany(mappedBy = "study", cascade = CascadeType.REMOVE, orphanRemoval = true)
+    private List<Notice> study_notices;
+
+    @Column
+    @ColumnDefault("null")
+    @OneToMany(mappedBy = "study", cascade = CascadeType.REMOVE, orphanRemoval = true)
+    private List<Promotion> study_promotions;
+
+    @Column
+    private Integer study_member_limit;
+
+    @Column
+    @ColumnDefault("true")
+    private boolean study_is_visible;
+
+    @Column
+    @ColumnDefault("true")
+    private boolean study_status;
+
+
 }
