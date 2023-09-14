170fce7b334ab7f1e2f6762204702a7105f38007
KoungQ <guu119@naver.com>
2023-09-14 20:25:00 +0900

    feat: User Entity 생성

--- src/main/java/com/mmos/mmos/src/domain/entity/User.java
+++ src/main/java/com/mmos/mmos/src/domain/entity/User.java
@@ -1,11 +1,10 @@
 package com.mmos.mmos.src.domain.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+import java.util.List;
 
 @Entity
 @Getter
@@ -16,5 +15,63 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long user_index;
 
-    
+    @Column
+    private String user_id;
+
+    @Column
+    private String user_password;
+
+    @Column
+    private String user_name;
+
+    @Column
+    private String user_nickname;
+
+    @Column
+    private String user_profile_image;
+
+    @Column
+    @ColumnDefault("0")
+    private Long user_total_study_time;
+
+    @Column
+    @ColumnDefault("0")
+    private Integer user_tier;
+
+    @Column
+    private Long user_student_id;
+
+    @Column
+    @ColumnDefault("0")
+    private Long user_total_completed_schedule_num;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.REMOVE, orphanRemoval = true)
+    private List<Planner> user_planners;
+
+    @Column
+    @ColumnDefault("null")
+    @ElementCollection
+    private List<Long> user_friends;
+
+    @Column
+    @ColumnDefault("null")
+    @ElementCollection
+    private List<Long> user_fixed_friends;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.REMOVE, orphanRemoval = true)
+    @ColumnDefault("null")
+    private List<UserBadge> user_userbadges;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.REMOVE, orphanRemoval = true)
+    @ColumnDefault("null")
+    private List<UserStudy> user_studies;
+
+    @Column
+    @ColumnDefault("true")
+    private boolean user_status;
+
+    @ManyToOne
+    @JoinColumn(name = "university_index")
+    private University university;
+
 }
