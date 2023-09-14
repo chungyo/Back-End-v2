1818357fbd62787ce993d2a6db9a4aab7d6a5c14
KoungQ <guu119@naver.com>
2023-09-14 16:06:56 +0900

    build: 초기 세팅

---
+++ src/main/java/com/mmos/mmos/MmosApplication.java
@@ -0,0 +1,13 @@
+package com.mmos.mmos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MmosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MmosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mmos/mmos/MmosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mmos.mmos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MmosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
