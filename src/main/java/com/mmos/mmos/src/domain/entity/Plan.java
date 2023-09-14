e4162d9422af3e92073920e987d2eddcb6cad393
Kim Junhyuk <jkim1202@gachon.ac.kr>
2023-09-14 19:24:32 +0900

    feat: Plan Entity 생성

--- src/main/java/com/mmos/mmos/src/domain/entity/Plan.java
+++ src/main/java/com/mmos/mmos/src/domain/entity/Plan.java
@@ -1,18 +1,52 @@
 package com.mmos.mmos.src.domain.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.DynamicInsert;
+
+import java.util.List;
 
 @Entity
 @Getter
 @NoArgsConstructor
+@DynamicInsert
 public class Plan {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long plan_index;
+
+    @Column
+    private String plan_name;
+
+    @Column
+    @ColumnDefault("true")
+    private boolean plan_is_complete;
+
+    @Column
+    @ColumnDefault("false")
+    private boolean plan_is_study;
+
+    @Column
+    @ColumnDefault("false")
+    private boolean plan_is_visible_on_calendar;
+
+    @OneToMany(mappedBy = "studytime", cascade = CascadeType.REMOVE, orphanRemoval = true)
+    private List<StudyTime> studytime_times;
+
+    @Column
+    @ColumnDefault("true")
+    private boolean studytime_status;
+
+    @ManyToOne
+    @JoinColumn(name = "planner_index")
+    private Planner planner;
+
+    @ManyToOne
+    @JoinColumn(name = "study_index")
+    private Study study;
+
+
 }
