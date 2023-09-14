cd3a10b5f4182ddf2077ad2b7acb58a9241ce380
KoungQ <guu119@naver.com>
2023-09-14 20:24:31 +0900

    feat: Post Entity 생성

--- src/main/java/com/mmos/mmos/src/domain/entity/Post.java
+++ src/main/java/com/mmos/mmos/src/domain/entity/Post.java
@@ -1,11 +1,11 @@
 package com.mmos.mmos.src.domain.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+
+import java.sql.Timestamp;
 
 @Entity
 @Getter
@@ -15,4 +15,39 @@ public class Post {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long post_index;
+
+    @Column
+    private String post_title;
+
+    @Column
+    private String post_contents;
+
+    @Column
+    private String post_image;
+
+    @Column
+    @ColumnDefault("true")
+    private boolean post_is_notice;
+
+    @Column
+    private String post_writer;
+
+    @Column
+    private Timestamp post_created_at;
+
+    @Column
+    private Timestamp post_updated_at;
+
+    @Column
+    @ColumnDefault("true")
+    private boolean post_status;
+
+    @ManyToOne
+    @JoinColumn(name = "notice_index")
+    private Notice notice;
+
+    @ManyToOne
+    @JoinColumn(name = "promotion_index")
+    private Promotion promotion;
+
 }
