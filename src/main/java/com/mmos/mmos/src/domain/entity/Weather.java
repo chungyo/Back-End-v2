22427bc02de77d50315d45513ec4d379c718c901
Kim Junhyuk <jkim1202@gachon.ac.kr>
2023-09-14 19:25:47 +0900

    feat: Weather Entity 생성

--- src/main/java/com/mmos/mmos/src/domain/entity/Weather.java
+++ src/main/java/com/mmos/mmos/src/domain/entity/Weather.java
@@ -1,18 +1,26 @@
 package com.mmos.mmos.src.domain.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.DynamicInsert;
 
 @Entity
 @Getter
 @NoArgsConstructor
+@DynamicInsert
 public class Weather {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long weather_index;
+
+    @Column
+    @ColumnDefault("맑음")
+    private String weather_image;
+
+    @Column
+    @ColumnDefault("true")
+    private boolean weather_status;
 }
