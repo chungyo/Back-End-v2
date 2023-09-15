c44cadaeb4bd70d9d31bea9269b996273aa4d1b3
KoungQ <guu119@naver.com>
2023-09-15 15:24:25 +0900

    refactor: Calendar 엔티티 구조 변경

--- src/main/java/com/mmos/mmos/src/domain/entity/Calendar.java
+++ src/main/java/com/mmos/mmos/src/domain/entity/Calendar.java
@@ -1,14 +1,18 @@
 package com.mmos.mmos.src.domain.entity;
 
 import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.DynamicInsert;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@ToString
 @Getter
 @NoArgsConstructor
 @DynamicInsert
@@ -19,9 +23,34 @@ public class Calendar {
     private Long calendar_index;
 
     @Column
+    private int calendar_month;
+
+    @Column
     @ColumnDefault("true")
     private boolean calendar_status;
 
+    @Column
+    @ColumnDefault("0")
+    private Long calendar_monthly_study_time;
+
+    @Column
+    @ColumnDefault("0")
+    private Long calendar_monthly_completed_plan_num;
+
     @OneToMany(mappedBy = "calendar", cascade = CascadeType.REMOVE, orphanRemoval = true)
-    private List<Planner> calendar_planners;
+    private List<Planner> calendar_planners = new ArrayList<>();
+
+    @ManyToOne
+    @JoinColumn(name = "user_index")
+    private User user;
+
+    @Builder
+    public Calendar(int month, User user) {
+        this.calendar_month = month;
+        this.user = user;
+    }
+
+    public void addPlanner(Planner planner) {
+        this.calendar_planners.add(planner);
+    }
 }
