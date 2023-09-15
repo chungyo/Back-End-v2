11986f600e92ff05524fec56645f0cdd8affce80
KoungQ <guu119@naver.com>
2023-09-15 15:28:23 +0900

    build: PlannerRepository 생성

---
+++ src/main/java/com/mmos/mmos/src/repository/PlannerRepository.java
@@ -0,0 +1,7 @@
+package com.mmos.mmos.src.repository;
+
+import com.mmos.mmos.src.domain.entity.Planner;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PlannerRepository extends JpaRepository<Planner, Long> {
+}
