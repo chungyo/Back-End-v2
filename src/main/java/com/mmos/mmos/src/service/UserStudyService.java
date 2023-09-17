6a0159352e853c1d3f0c986584db11ac5a5c4574
chungyoung kim <your-email-kimchoungy@gachon.ac.kr>
2023-09-17 19:37:04 +0900

    feat: saveUserStudy 기능 추가

--- src/main/java/com/mmos/mmos/src/service/UserStudyService.java
+++ src/main/java/com/mmos/mmos/src/service/UserStudyService.java
@@ -25,14 +25,36 @@ public class UserStudyService {
                 .orElseThrow(()->new IllegalArgumentException("존재하지 않는 유저입니다. STUDY_INDEX = " + userIdx));
     }
 
+    // 해당 스터디에 가입된 유저인지 확인
+    public boolean findUserIsAlreadyExist(Long userIdx, Long studyIdx){
+        User user = findUser(userIdx);
+        for(UserStudy userStudy : user.getUser_userstudies()) {
+            if(userStudy.getStudy().getStudy_index().equals(studyIdx))
+                return true;
+        }
+        return false;
+    }
+
+    // 새로운 스터디원 등록
     @Transactional
     public UserStudy saveUserStudy(boolean isLeader, Long studyIdx, Long userIdx) {
         Study study = findStudy(studyIdx);
         User user = findUser(userIdx);
+
+        // 이미 가득찬 스터디인지 확인
+        if(study.getStudy_userstudies().size() == study.getStudy_member_limit()){
+            throw new IllegalArgumentException("이미 가득찬 스터디입니다.");
+        }
+
+        // 이미 스터디원으로 등록되어 있는지 확인
+        if (!isLeader && findUserIsAlreadyExist(userIdx, studyIdx)) {
+            throw new IllegalArgumentException("이미 스터디원으로 등록된 사용자입니다.");
+        }
         UserStudy userStudy= new UserStudy(isLeader, user,study);
         study.addUserStudy(userStudy);
         return userStudyRepository.save(userStudy);
     }
+
 
 
 }
