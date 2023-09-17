dfef73c692534bcea69f23471a57c908c6638e9c
Kim Junhyuk <jkim1202@gachon.ac.kr>
2023-09-17 13:13:59 +0900

    feat: PromotionRepository 생성

---
+++ src/main/java/com/mmos/mmos/src/repository/PromotionRepository.java
@@ -0,0 +1,7 @@
+package com.mmos.mmos.src.repository;
+
+import com.mmos.mmos.src.domain.entity.Promotion;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PromotionRepository extends JpaRepository<Promotion, Long> {
+}
