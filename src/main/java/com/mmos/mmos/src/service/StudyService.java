a1357153a2b08b452d60067a235fbfeababc06b3
Kim Junhyuk <jkim1202@gachon.ac.kr>
2023-09-17 13:20:23 +0900

    feat: StudyService 생성

---
+++ src/main/java/com/mmos/mmos/src/service/StudyService.java
@@ -0,0 +1,32 @@
+package com.mmos.mmos.src.service;
+
+import com.mmos.mmos.src.domain.dto.study.StudySaveRequestDto;
+import com.mmos.mmos.src.domain.entity.Study;
+import com.mmos.mmos.src.repository.StudyRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class StudyService {
+    private final StudyRepository studyRepository;
+
+    // 스터디 생성
+    @Transactional
+    public Study saveStudy(StudySaveRequestDto requestDto){
+        Study study = requestDto.toEntity();
+        
+        return studyRepository.save(study);
+    }
+
+    public Study findStudy(Long studyIdx){
+        return studyRepository.findById(studyIdx)
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 스터디입니다. STUDY_INDEX" + studyIdx));
+    }
+    @Transactional
+    public void updateStudyName(Long studyIdx, String name) {
+        Study study = findStudy(studyIdx);
+        study.setStudy_name(name);
+    }
+}
