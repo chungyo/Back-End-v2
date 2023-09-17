bee50080962496bf9ced29a57b2442bb12590034
KoungQ <guu119@naver.com>
2023-09-17 13:25:17 +0900

    feat: 비밀번호 변경, 닉네임 변경, 프로필 이미지 변경 기능 추가

--- src/main/java/com/mmos/mmos/src/service/UserService.java
+++ src/main/java/com/mmos/mmos/src/service/UserService.java
@@ -1,5 +1,7 @@
 package com.mmos.mmos.src.service;
 
+import com.mmos.mmos.src.domain.dto.user.UserNicknameUpdateDto;
+import com.mmos.mmos.src.domain.dto.user.UserPwdUpdateDto;
 import com.mmos.mmos.src.domain.dto.user.UserSaveRequestDto;
 import com.mmos.mmos.src.domain.entity.User;
 import com.mmos.mmos.src.repository.UserRepository;
@@ -24,6 +26,33 @@ public class UserService {
         User user = requestDto.toEntity();
 
         return userRepository.save(user);
+    }
+
+    // 비밀번호 변경
+    @Transactional
+    public void updatePwd(UserPwdUpdateDto userPwdUpdateDto, Long userIdx) {
+        // 유저 정보 찾기
+        User user = findUser(userIdx);
+        // 유저 비밀번호 변경
+        user.updatePwd(userPwdUpdateDto.getNewPwd());
+    }
+
+    // 닉네임 변경
+    @Transactional
+    public void updateNickname(UserNicknameUpdateDto userNicknameUpdateDto, Long userIdx) {
+        // 유저 정보 찾기
+        User user = findUser(userIdx);
+        // 유저 비밀번호 변경
+        user.updateNickname(userNicknameUpdateDto.getNewNickname());
+    }
+
+    // 프로필 이미지 변경
+    @Transactional
+    public void updatePfp(String pfp, Long userIdx) {
+        // 유저 정보 찾기
+        User user = findUser(userIdx);
+        // 유저 프로필 이미지 변경
+        user.updatePfp(pfp);
     }
 
 }
