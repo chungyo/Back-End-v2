2a068f799e5eb0d2c09368dad688b274ab577c5c
KoungQ <guu119@naver.com>
2023-09-17 16:55:44 +0900

    build: Tier TierRepository 생성

---
+++ src/main/java/com/mmos/mmos/src/repository/TierRepository.java
@@ -0,0 +1,7 @@
+package com.mmos.mmos.src.repository;
+
+import com.mmos.mmos.src.domain.entity.Tier;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TierRepository extends JpaRepository<Tier, Long> {
+}
