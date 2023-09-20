1e1e557a8f937df63435acf2e4e04ee7b0f64527
KoungQ <guu119@naver.com>
2023-09-20 15:07:44 +0900

    DELETE

--- src/main/java/com/mmos/mmos/src/repository/NoticeRepositoty.java
+++
@@ -1,7 +0,0 @@
-package com.mmos.mmos.src.repository;
-
-import com.mmos.mmos.src.domain.entity.Notice;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NoticeRepositoty extends JpaRepository<Notice, Long> {
-}
--- src/main/java/com/mmos/mmos/src/repository/PromotionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.mmos.mmos.src.repository;
-
-import com.mmos.mmos.src.domain.entity.Promotion;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PromotionRepository extends JpaRepository<Promotion, Long> {
-}
