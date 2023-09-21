b971a1951db5d018f60c3f295db7fe56c7efe439
KoungQ <guu119@naver.com>
2023-09-21 17:39:21 +0900

    build: MajorRepository 생성

---
+++ src/main/java/com/mmos/mmos/src/repository/MajorRepository.java
@@ -0,0 +1,7 @@
+package com.mmos.mmos.src.repository;
+
+import com.mmos.mmos.src.domain.entity.Major;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MajorRepository extends JpaRepository<Major, Long> {
+}
