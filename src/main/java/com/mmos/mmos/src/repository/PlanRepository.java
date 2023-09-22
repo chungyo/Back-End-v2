b0729b49d4c658b17380c3bec33eb9847002c21d
chungyoung kim <your-email-kimchoungy@gachon.ac.kr>
2023-09-22 20:44:49 +0900

    feat: findPlansByPlanIsStudy 기능 추가

--- src/main/java/com/mmos/mmos/src/repository/PlanRepository.java
+++ src/main/java/com/mmos/mmos/src/repository/PlanRepository.java
@@ -3,5 +3,9 @@ package com.mmos.mmos.src.repository;
 import com.mmos.mmos.src.domain.entity.Plan;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface PlanRepository extends JpaRepository<Plan, Long> {
+    Optional<List<Plan>> findPlansByPlanIsStudy(Boolean isStudy);
 }
