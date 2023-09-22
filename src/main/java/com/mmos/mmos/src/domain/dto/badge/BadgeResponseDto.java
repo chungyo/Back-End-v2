9b9d8565f894e58e06581c7be467f642ec13844a
chungyoung kim <your-email-kimchoungy@gachon.ac.kr>
2023-09-22 17:50:37 +0900

    refactor: DTO 수정

--- src/main/java/com/mmos/mmos/src/domain/dto/badge/BadgeResponseDto.java
+++ src/main/java/com/mmos/mmos/src/domain/dto/badge/BadgeResponseDto.java
@@ -7,19 +7,19 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public class BadgeResponseDto {
-    private Long badgeIdx;
-    private Long badgeExp;
-    private String badgeIcon;
-    private String badgeInfo;
-    private String badgeName;
-    private Boolean badgeStatus;
+    private Long idx;
+    private Long exp;
+    private String icon;
+    private String info;
+    private String name;
+
 
 
     public BadgeResponseDto(Badge entity) {
-        this.badgeIdx = entity.getBadgeIndex();
-        this.badgeExp = entity.getBadgeExp();
-        this.badgeIcon = entity.getBadgeIcon();
-        this.badgeInfo = entity.getBadgeInfo();
-        this.badgeName = entity.getBadgeName();
+        this.idx = entity.getBadgeIndex();
+        this.exp = entity.getBadgeExp();
+        this.icon = entity.getBadgeIcon();
+        this.info = entity.getBadgeInfo();
+        this.name = entity.getBadgeName();
     }
 }
