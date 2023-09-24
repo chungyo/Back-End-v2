a3844d9da7bdd2ccf1789cd91b67941cf95ec115
chungyoung kim <your-email-kimchoungy@gachon.ac.kr>
2023-09-24 18:55:56 +0900

    feat: findPostByPostIsNotice 기능 추가

--- src/main/java/com/mmos/mmos/src/repository/PostRepository.java
+++ src/main/java/com/mmos/mmos/src/repository/PostRepository.java
@@ -3,5 +3,9 @@ package com.mmos.mmos.src.repository;
 import com.mmos.mmos.src.domain.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface PostRepository extends JpaRepository<Post, Long> {
+    Optional<List<Post>> findPostsByPostIsNotice(Boolean postIsNotice);
 }
