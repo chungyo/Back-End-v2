deae636d0054f8c9534ca3c21a8fdaa5dc099184
KoungQ <guu119@naver.com>
2023-09-25 13:51:17 +0900

    점검 완료

---
+++ src/main/java/com/mmos/mmos/src/domain/dto/planner/PlannerUpdateMemoRequestDto.java
@@ -0,0 +1,11 @@
+package com.mmos.mmos.src.domain.dto.planner;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class PlannerUpdateMemoRequestDto {
+
+    private String memo;
+}
