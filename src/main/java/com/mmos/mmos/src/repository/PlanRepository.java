f6a52600e6b06b67b669d1a5229ac55e3711bf59
KoungQ <guu119@naver.com>
2023-09-27 12:03:45 +0900

    Revert "feat: Planner 객체 반환 코드 추가"
    This reverts commit 68e00241a547c931fdbc11c385f817210b3aa4e6.

--- src/main/java/com/mmos/mmos/src/repository/PlanRepository.java
+++ src/main/java/com/mmos/mmos/src/repository/PlanRepository.java
@@ -1,14 +1,11 @@
 package com.mmos.mmos.src.repository;
 
 import com.mmos.mmos.src.domain.entity.Plan;
-import com.mmos.mmos.src.domain.entity.Planner;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface PlanRepository extends JpaRepository<Plan, Long> {
-    Optional<List<Plan>> findPlansByPlannerAndPlanIsStudy(Planner planner, Boolean isStudy);
-
-    Optional<List<Plan>> findPlansByPlanner(Planner planner);
+    Optional<List<Plan>> findPlansByPlanIsStudy(Boolean isStudy);
 }
