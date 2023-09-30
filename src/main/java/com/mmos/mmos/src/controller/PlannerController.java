d3f5bbe6f45f3aa092dd71a75402d73480269d4f
chungyoung kim <your-email-kimchoungy@gachon.ac.kr>
2023-09-30 17:00:04 +0900

    feat: getPlanner 기능 추가

--- src/main/java/com/mmos/mmos/src/controller/PlannerController.java
+++ src/main/java/com/mmos/mmos/src/controller/PlannerController.java
@@ -3,6 +3,7 @@ package com.mmos.mmos.src.controller;
 import com.mmos.mmos.config.ResponseApiMessage;
 import com.mmos.mmos.src.domain.dto.planner.PlannerResponseDto;
 import com.mmos.mmos.src.domain.dto.planner.PlannerUpdateMemoRequestDto;
+import com.mmos.mmos.src.service.PlanService;
 import com.mmos.mmos.src.service.PlannerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +17,7 @@ import static com.mmos.mmos.config.HttpResponseStatus.SUCCESS;
 public class PlannerController extends BaseController {
 
     private final PlannerService plannerService;
+    private final PlanService planService;
 
     /**
      * 플래너 메모 추가 기능 (완료)
@@ -25,12 +27,24 @@ public class PlannerController extends BaseController {
      */
     // 플래너 메모 기능
     @ResponseBody
-    @PatchMapping("/{plannerIdx}")
+    @PatchMapping("memo/{plannerIdx}")
     public ResponseEntity<ResponseApiMessage> updatePlannerMemo(@RequestBody PlannerUpdateMemoRequestDto requestDto, @PathVariable Long plannerIdx){
          PlannerResponseDto responseDto = plannerService.setMemo(plannerIdx,requestDto.getMemo());
 
          return sendResponseHttpByJson(SUCCESS, "UPDATE PLANNER_MEMO_COMPLETE. PLANNER_INDEX=" + plannerIdx, responseDto);
 
     }
+
+    /**
+     * 플래너와 관련된 모든 것 가져오는 기능 (계획들, 프로젝트(추가 예정), 메모, 날짜, 총 공부시간, 총 공부량)
+     * @param plannerIdx: 플래너 인덱스
+     */
+    @ResponseBody
+    @GetMapping("/all/{plannerIdx}")
+    public ResponseEntity<ResponseApiMessage> getPlanner(@PathVariable Long plannerIdx) {
+        PlannerResponseDto responseDtoList = plannerService.getPlanner(plannerIdx);
+        return sendResponseHttpByJson(SUCCESS, "GET PLANNER COMPLETE. PLANNER_INDEX=" + plannerIdx, responseDtoList);
+    }
+
 
 }
