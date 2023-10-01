b2d7d4fd95c509b76d87e27fed7af9e9e85e0d14
chungyoung kim <your-email-kimchoungy@gachon.ac.kr>
2023-10-01 15:28:22 +0900

    refactor: status, project 추가

--- src/main/java/com/mmos/mmos/src/domain/dto/planner/PlannerResponseDto.java
+++ src/main/java/com/mmos/mmos/src/domain/dto/planner/PlannerResponseDto.java
@@ -1,7 +1,9 @@
 package com.mmos.mmos.src.domain.dto.planner;
 
+import com.mmos.mmos.config.HttpResponseStatus;
 import com.mmos.mmos.src.domain.entity.Plan;
 import com.mmos.mmos.src.domain.entity.Planner;
+import com.mmos.mmos.src.domain.entity.Project;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -18,14 +20,31 @@ public class PlannerResponseDto {
     private Long dailyStudyTime;
     private Long dailyScheduleNum;
     private List<Plan> plans;
+    private List<Project> projects;
+    private HttpResponseStatus status = null;
 
-    public PlannerResponseDto(Planner planner) {
+    public PlannerResponseDto(Planner planner, List<Project> projects, HttpResponseStatus status) {
         this.idx = planner.getPlannerIndex();
         this.date = planner.getPlannerDate();
         this.memo = planner.getPlannerMemo();
         this.dailyStudyTime = planner.getPlannerDailyStudyTime();
         this.dailyScheduleNum = planner.getPlannerDailyScheduleNum();
+        this.projects = projects;
+        this.status = status;
         this.plans = planner.getPlannerPlans();
 
+
+    }
+
+
+    public PlannerResponseDto(HttpResponseStatus status){
+        this.idx = null;
+        this.date = null;
+        this.memo = null;
+        this.dailyStudyTime = null;
+        this.dailyScheduleNum = null;
+        this.status = status;
+        this.projects = null;
+        this.plans = null;
     }
 }
