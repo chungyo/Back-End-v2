7f7fbae3d8fd0fa98af488257d29773f8277fd17
chungyoung kim <your-email-kimchoungy@gachon.ac.kr>
2023-10-01 19:50:36 +0900

    feat: 프로젝트 개수 세는 기능 추가

--- src/main/java/com/mmos/mmos/src/repository/ProjectRepository.java
+++ src/main/java/com/mmos/mmos/src/repository/ProjectRepository.java
@@ -5,8 +5,9 @@ import com.mmos.mmos.src.domain.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface ProjectRepository extends JpaRepository<Project,Long> {
     List<Project> findAllByUser(User user);
+
+    Long countByUserAndProjectIsVisibleTrue(User user);
 }
